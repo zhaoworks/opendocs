f67ef983e467d784f27d0ae7ed9672a96677c4d5
shwuy <itallo@zhaoworks.com>
2025-05-10 18:33:43 -0300

    feat: edit on github button

--- app/(docs)/[[...slug]]/page.tsx
+++ app/(docs)/[[...slug]]/page.tsx
@@ -3,10 +3,12 @@ import {
   DocsPage,
   DocsBody,
   DocsTitle,
+  EditOnGitHub,
 } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
 import { createRelativeLink } from 'fumadocs-ui/mdx';
 import { getMDXComponents } from '@/mdx-components';
+import { PenBoxIcon } from 'lucide-react';
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
@@ -19,7 +21,15 @@ export default async function Page(props: {
 
   return (
     <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
+      <div className="flex md:flex-row flex-col md:items-center gap-3 justify-between">
+        <DocsTitle className="truncate">{page.data.title}</DocsTitle>
+        <div>
+          <EditOnGitHub href={`https://github.com/zhaoworks/opendocs/edit/main/content/docs/${page.file.path}`}>
+            <PenBoxIcon size={14} />
+            <span>Editar no GitHub</span>
+          </EditOnGitHub>
+        </div>
+      </div>
       <DocsBody>
         <MDXContent
           components={getMDXComponents({
